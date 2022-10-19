b3ab94cf1c071f8db00099aac50500df37d4c3c8
Dafarlog <Dafarlog@gmail.com>
2022-10-19 14:40:04 +0300

    ref to hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,12 +3,7 @@ import { Feedback } from 'components/Feedback/Feedback';
 export const App = () => {
   return (
     <>
-      <Feedback
-        feedbackTitle="Leave feedback"
-        statisticTitle="Statistic"
-        feedbackOptions={['Good', 'Neutral', 'Bad']}
-        percentageOptions={['Good', 'Bad']}
-      />
+      <Feedback />
     </>
   );
 };
--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,60 +1,51 @@
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 
 import { Button } from 'components/Button/Button';
 import { Statistics } from 'components/Statistics/Statistics';
 import { Section } from 'components/Section/Section';
 import React from 'react';
 
-export class Feedback extends React.Component{
+export const Feedback = props => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-    constructor(props){
-        super(props);
-        const state = this.props.feedbackOptions.reduce((acc, elem) =>{
-            const obj = {[elem]: 0}
-            return{...acc, ...obj}
-        }, {});
-        this.state = {...state};
-    }
-    onLeaveFeedback = (e) => {
-        this.setState(state => ({ 
-            [e.target.textContent]: state[e.target.textContent]+1, 
-        }))
-    }
+  const handleGoodChange = event => {
+    setGood(good + 1);
+  };
+  const handleNeutralChange = event => {
+    setNeutral(neutral + 1);
+  };
+  const handleBadChange = event => {
+    setBad(bad + 1);
+  };
 
-    percentage = (id) => {
-        const total = Object.values(this.state).reduce((acc, elem)=>{
-            return acc+elem;
-        }, 0);
-        if(total <= 0) return 0; 
-        return Math.round(this.state[id]/total*10000)/100 ;
-    }
-    render(){
-        return(
-        <>
-        <Section title={this.props.feedbackTitle}>
-            <div className='feedback-wrap'>
-                {this.props.feedbackOptions.map(elem=>(
-                    <Button key ={elem} func = {this.onLeaveFeedback} text = {elem}/>
-                ))}
-            </div>
-        </Section>
-        
-        <Section title={this.props.statisticTitle} >
-            <Statistics  
-                names={Object.keys(this.state)}
-                values={Object.values(this.state)}
-                percentageOptions = {{iDs: this.props.percentageOptions, func: this.percentage}}  />
-        </Section>
-        
-        </>
-    );
-    }
-    
-}
+  const goodPercentage = id => {
+    const total = good + neutral + bad;
+    if (total <= 0) return 0;
+    return Math.round((good / total) * 10000) / 100;
+  };
 
-Feedback.propTypes = {
-    statisticTitle: PropTypes.string,
-    feedbackTitle: PropTypes.string,
-    feedbackOptions: PropTypes.arrayOf(PropTypes.string,).isRequired,
-    percentageOptions: PropTypes.arrayOf(PropTypes.string,),
-}
+  return (
+    <>
+      <Section title="Leave feedback">
+        <div className="feedback-wrap">
+          <Button func={handleGoodChange} text="Good" />
+          <Button func={handleNeutralChange} text="neutral" />
+          <Button func={handleBadChange} text="bad" />
+        </div>
+      </Section>
+
+      <Section title="Statistic">
+        <Statistics
+          name={'good'}
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          percentage={goodPercentage()}
+        />
+      </Section>
+    </>
+  );
+};
